72e567bed06fe42c01f086223ea3f495d6c23da9
Ashish <sainiashish356@gmail.com>
2022-06-28 18:12:21 +0530

    added banner component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar';
+import './App.css';
+import Banner from './Components/Banner';
+
+function App() {
+  return (
+    <>
+      <Navbar /> 
+      <Banner />
+    </>
+  );
+}
+
+export default App;
