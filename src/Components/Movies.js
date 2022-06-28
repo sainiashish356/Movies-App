b028aceac9e8bb4ff614468207ab93b8af0b1b66
Ashish <sainiashish356@gmail.com>
2022-06-29 04:41:47 +0530

    fetching movies via api using axios

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,5 +1,6 @@
-import { movies } from "./getMovies";
-import React, { Component } from 'react'
+// import { movies } from "./getMovies";
+import React, { Component } from 'react';
+import axios from "axios";
 
 export default class Movies extends Component {
   constructor(){
@@ -7,15 +8,56 @@ export default class Movies extends Component {
      this.state={
       hover: '',
       parr:[1],
+      currPage: 1,
+      movies:[],
      }
   }
 
+  async componentDidMount(){
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5441569e0ccf4e14443bf3081bd0d48f&language=en-US&page=${this.state.currPage}`);
+    let data = res.data;
+
+    this.setState({
+      movies:[...data.results]
+    })
+    console.log(data);
+  }
+
+  changeMovies = async () => {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5441569e0ccf4e14443bf3081bd0d48f&language=en-US&page=${this.state.currPage}`);
+    let data = res.data;
+
+    this.setState({
+      movies:[...data.results]
+    })
+  }
+
+  handleRight =() => {
+    let temparr = [];
+    for(let i = 1;i <= this.state.parr.length+1;i++){
+      temparr.push(i);
+    }
+    this.setState({
+      parr:[...temparr],
+      currPage:this.state.currPage+1,
+    } , this.changeMovies)
+  }
+   
+  handlePrev = () => {
+   if(this.state.currPage != 1){
+    this.setState({
+        currPage: this.state.currPage-1
+    } , this.changeMovies)
+   }
+  }
+
+
   render() {
-    let movie = movies.results;
+    // let movie = movies.results;
     return (
       <>
         {
-            movie.length == 0 ?
+            this.state.movies.length == 0 ?
             <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
                 </div> : 
@@ -23,9 +65,9 @@ export default class Movies extends Component {
                     <h3 className="text-center"><strong>Trending</strong></h3>
                     <div className="movies-list">
                         {
-                            movie.map((movObj) => (
+                            this.state.movies.map((movObj) => (
                                 <div className="card movies-card" onMouseEnter={() => this.setState({hover:movObj.id})} onMouseLeave={() => this.setState({hover: ''})}>
-                                <img src={`https://image.tmdb.org/t/p/original${movObj.backdrop_path}`} class="card-img-top banner-img movies-img" alt={movObj.title} />
+                                <img src={`https://image.tmdb.org/t/p/original${movObj.backdrop_path}`} className="card-img-top banner-img movies-img" alt={movObj.title} />
                                 {/* <div className="card-body"> */}
                                     <h4 className="card-title movies-title">{movObj.original_title}</h4>
                                     {/* <p className="card-text movies-text"> {movObj.overview}</p> */}
@@ -46,7 +88,7 @@ export default class Movies extends Component {
                          <div style={{justifyContent:'center' , display:'flex'}}>
                               <nav aria-label="...">
                               <ul class="pagination">
-                              <a class="page-link">Previous</a>
+                              <a className="page-link prev-btn" onClick={this.handlePrev}>Previous</a>
                               {
                                 this.state.parr.map((value) => (
                                   <li class="page-item disabled"> <a class="page-link" href="#">{value}</a> </li>
@@ -54,7 +96,7 @@ export default class Movies extends Component {
                               }
                                
                                 <li class="page-item">
-                                  <a class="page-link" href="#">Next</a>
+                                  <a className="page-link nxt-btn" onClick={this.handleRight}>Next</a>
                                 </li>
                               </ul>
                             </nav>
